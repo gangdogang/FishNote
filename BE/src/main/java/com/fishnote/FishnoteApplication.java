11d4112ae635d5027febf56223fdc1803fd484f3
ICN <icn20260690@gmail.com>
2026-07-28 03:22:06 +0900

    [fix] fit backend within Render free memory

--- BE/src/main/java/com/fishnote/FishnoteApplication.java
+++ BE/src/main/java/com/fishnote/FishnoteApplication.java
@@ -2,10 +2,13 @@ package com.fishnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+// 인증은 SecurityConfig의 stateless JWT 체인만 사용한다. 기본 in-memory 사용자를 만들면
+// 운영 로그에 일회성 비밀번호가 노출되고 불필요한 보안 객체가 메모리를 차지한다.
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableScheduling
 @EnableCaching
 public class FishnoteApplication {
